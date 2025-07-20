ef27d4ed678140872b26c6bb92da17cbaba0c039
Rafael Grigorian <rafael@grigorian.org>
2025-07-19 23:46:21 -0500

    Removed last-used command and made it a flag in select command

--- internal/select.go
+++ internal/select.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/null93/aws-knox/sdk/credentials"
+	"github.com/null93/aws-knox/sdk/tui"
 	"github.com/spf13/cobra"
 )
 
@@ -19,35 +20,71 @@ var selectCmd = &cobra.Command{
 	},
 	Run: func(cmd *cobra.Command, args []string) {
 		var role *credentials.Role
+		var lastRole credentials.Role
 		var action string
+		var sessions credentials.Sessions
+		var session *credentials.Session
+		var err error
+
 		for {
-			if !selectCachedFirst || (sessionName != "" && accountId != "" && roleName != "") {
-				action, role = SelectRoleCredentialsStartingFromSession()
-			} else {
-				action, role = SelectRoleCredentialsStartingFromCache()
-			}
-			if action == "toggle-view" {
-				toggleView()
-				continue
-			}
-			if action == "back" {
-				goBack(&role)
-				continue
-			}
-			if action == "delete" {
-				if role != nil && role.Credentials != nil {
-					role.Credentials.DeleteCache(role.SessionName, role.CacheKey())
-					role = nil
+			if lastUsed {
+				if lastRole, err = credentials.GetLastUsedRole(); err != nil {
+					ExitWithError(1, "failed to get last used role", err)
 				}
-				continue
+				if lastRole.Credentials == nil || lastRole.Credentials.IsExpired() {
+					if sessions, err = credentials.GetSessions(); err != nil {
+						ExitWithError(2, "failed to parse sso sessions", err)
+					}
+					if session = sessions.FindByName(lastRole.SessionName); session == nil {
+						ExitWithError(3, "failed to find sso session "+lastRole.SessionName, err)
+					}
+					if session.ClientToken == nil || session.ClientToken.IsExpired() {
+						if err = tui.ClientLogin(session); err != nil {
+							ExitWithError(4, "failed to authorize device login", err)
+						}
+					}
+					if err = session.RefreshRoleCredentials(&lastRole); err != nil {
+						ExitWithError(5, "failed to get credentials", err)
+					}
+					if !doNotCache {
+						if err = lastRole.Credentials.Save(session.Name, lastRole.CacheKey()); err != nil {
+							ExitWithError(6, "failed to save credentials", err)
+						}
+					}
+				}
+			} else {
+				if !selectCachedFirst || (sessionName != "" && accountId != "" && roleName != "") {
+					action, role = SelectRoleCredentialsStartingFromSession()
+				} else {
+					action, role = SelectRoleCredentialsStartingFromCache()
+				}
+				if action == "toggle-view" {
+					toggleView()
+					continue
+				}
+				if action == "back" {
+					goBack(&role)
+					continue
+				}
+				if action == "delete" {
+					if role != nil && role.Credentials != nil {
+						role.Credentials.DeleteCache(role.SessionName, role.CacheKey())
+						role = nil
+					}
+					continue
+				}
+			}
+			targetRole := role
+			if lastUsed {
+				targetRole = &lastRole
 			}
 			if format == "env" {
-				fmt.Printf("export AWS_ACCESS_KEY_ID=%q\n", role.Credentials.AccessKeyId)
-				fmt.Printf("export AWS_SECRET_ACCESS_KEY=%q\n", role.Credentials.SecretAccessKey)
-				fmt.Printf("export AWS_SESSION_TOKEN=%q\n", role.Credentials.SessionToken)
+				fmt.Printf("export AWS_ACCESS_KEY_ID=%q\n", targetRole.Credentials.AccessKeyId)
+				fmt.Printf("export AWS_SECRET_ACCESS_KEY=%q\n", targetRole.Credentials.SecretAccessKey)
+				fmt.Printf("export AWS_SESSION_TOKEN=%q\n", targetRole.Credentials.SessionToken)
 				break
 			} else {
-				if json, err := role.Credentials.ToJSON(); err != nil {
+				if json, err := targetRole.Credentials.ToJSON(); err != nil {
 					ExitWithError(19, "failed to convert credentials to json", err)
 				} else {
 					fmt.Println(json)
@@ -66,4 +103,5 @@ func init() {
 	selectCmd.Flags().StringVarP(&accountId, "account-id", "a", accountId, "AWS account ID")
 	selectCmd.Flags().StringVarP(&roleName, "role-name", "r", roleName, "AWS role name")
 	selectCmd.Flags().BoolVarP(&doNotCache, "no-cache", "n", doNotCache, "Do not cache credentials")
+	selectCmd.Flags().BoolVarP(&lastUsed, "last-used", "l", lastUsed, "Use last used role credentials")
 }
